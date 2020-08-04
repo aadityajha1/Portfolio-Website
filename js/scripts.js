2b4fd49c7dff657bc8b9cf230c80784e22279214
Aaditya jha <aadityajha2000@gmail.com>
2020-08-04 19:08:51 +0545

    Contact Modal added

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,7 @@
 $(document).ready(function(){
     // alert('Working');
-    
+    $('#contactbtn').click(function(){
+        // alert('Im licked');
+        $('#contactModal').modal('show');
+    });
 })
